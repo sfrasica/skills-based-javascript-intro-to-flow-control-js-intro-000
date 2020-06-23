aa92983fc574fa354876e273d2ef65109cac6a59
Steven Frasica <sfrasica1@gmail.com>
2020-06-23 20:54:38 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -30,6 +30,10 @@ function ternaryTeenager(age) {
 function switchAge(age) {
   switch(age) {
     case age >= 13 && age <= 19:
-      "You are a teenager"
+      "You are a teenager";
+        break;
+    default:
+      "You have an age";
+      
   }
 }
