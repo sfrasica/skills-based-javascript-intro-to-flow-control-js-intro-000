6bbcbfe11be6888f47aad6b32ea167839c93afaf
Steven Frasica <sfrasica1@gmail.com>
2020-06-23 20:30:11 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,5 +1,5 @@
 function basicTeenager(age) {
-   return 13 <= age <= 19 ? "You are a teenager!" : "You are a kid"
+   return 13 <= age <= 19 ? "You are a teenager!" : undefined
 }
 
 function teenager(age) {
